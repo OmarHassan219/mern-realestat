3d945cf2f75815da71ef7063db3c6bef6a54daa7
OmarHassan219 <ramo-3000@live.com>
2023-11-18 16:17:17 +0400

    complete delete userListing fun

--- api/controller/listing.controller.js
+++ api/controller/listing.controller.js
@@ -1,4 +1,5 @@
 import listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 
 export const createListing = async (req ,res ,next) => {
@@ -25,5 +26,34 @@ try {
 
 
 
+export const deleteListing = async (req, res, next) => {
 
+    const findList = await listing.findById(req.params.id) ;
+    if(!findList) return next(errorHandler(401 , 'listing Not Found'));
+    
+    if(findList.userRef === req.user.id)  return next(errorHandler(401 , 'you can only delete your listings'));
+    
+    try {
+        await listing.findByIdAndDelete( req.params.id) ;
+    return res.status(200).json('Listing has been deleted')
+} catch (error) {
+   next(error) 
+}
+
+
+
+
+// try {
+// const findList = await listing.findById(req.params.id) ;
+// if(findList.userRef === req.user.id) {
+//  await listing.deleteOne({_id : req.params.id}) ;
+// return res.status(201).json('deleted successfully')
+// }
+// next(errorHandler(401 , 'you can only delete your listings'))
+// } catch (error) {
+//    next(error) 
+// }
+
+
+}
 
--- realestate/src/pages/Profile.jsx
+++ realestate/src/pages/Profile.jsx
@@ -155,6 +155,63 @@ const Profile = () => {
     }
   };
 
+
+const handleDelete = async(id) =>{
+
+
+try {
+  const res = await fetch(`/api/listing/delete/${id}`, {
+    method:'DELETE'
+  });
+const data = await res.json();
+if(data.success === false){
+  console.log(data.message);
+  return;
+}
+console.log(data);
+setUserListings(prev => prev.filter((listing) => listing._id !== id))
+// handleShowListings()
+} catch (error) {
+console.log(error.message);
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -245,11 +302,12 @@ const Profile = () => {
         {showListingsError ? "Error showing listings " : ""}
       </p>
       {userListings &&
-        userListings.length > 0 &&
+        userListings.length > 0 ?
+        (
         <div className="flex flex-col gap-4">
           <h1 className="text-center mt-7 text-2xl font-semibold ">Your Listings</h1>
          { userListings.map((listing) => (
-            <div key={listing._id} className="border rounded-lg p-3">
+            <div key={listing._id} className="border flex gap-4 justify-between items-center rounded-lg p-3">
               <Link to={`/listing/${listing._id}`} className="flex gap-4 justify-between items-center">
                 <img
                   src={listing.imageUrls[0]}
@@ -257,15 +315,16 @@ const Profile = () => {
                   className="h-16 w-16 object-contain "
                 />
                 <p className="text-slate-700 font-semibold flex-1 hover:underline truncate">{listing.name}</p>
+              </Link>
                 <div className="flex flex-col items-center">
-  <button className="text-red-700 uppercase">Delete</button>
+  <button onClick={() => handleDelete(listing._id)} className="text-red-700 uppercase">Delete</button>
   <button className="text-red-700 uppercase">Edit</button>
                 </div>
-              </Link>
             </div>
           ))}
 
-        </div>}
+        </div>): <p className="text-red-700">No Listings to show. Try to add some</p>
+}
     </div>
   );
 };
